63272a8ba1d1e5cb34c3cbae73614a50ca5dd15e
Tristan Slater <1631008+trslater@users.noreply.github.com>
2024-02-25 14:13:31 -0800

    Get username and password from environment

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -2,8 +2,8 @@ import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
   await page.goto("https://trslater.ca/learn-playwright/");
-  await page.getByLabel("Username:").fill("user1");
-  await page.getByLabel("Password:").fill("pass1");
+  await page.getByLabel("Username:").fill(process.env.FAKE_USERNAME ?? "");
+  await page.getByLabel("Password:").fill(process.env.FAKE_PASSWORD ?? "");
   await page.getByRole("button", { name: "Login" }).click();
   await expect(page.getByText("Foobar")).toBeVisible();
 });
